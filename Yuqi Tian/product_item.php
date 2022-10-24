a4bfacf00aeb505e90b9c126aabb3350dc874d1e
yui19940424 <108223585+yui19940424@users.noreply.github.com>
2022-10-24 14:16:25 +0800

    Add files via upload

---
+++ Yuqi Tian/product_item.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Product Item</title>
+
+	<meta name="viewport"content="width=device-width">
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+</head>
+<body>
+    
+    <?php include"parts/navbar.php";?>
+
+	<div class="contianer">
+		<div class="card soft">
+			<h2>Product Item</h2>
+
+			<p>This is item # </p>
+
+		</div>
+	</div>
+
+</body>
+</html>
